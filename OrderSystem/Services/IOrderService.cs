dbcdf888bf64a7b0cfcf7c484bef10b2f1d12d90
nooshinjavadzade <javadzadenooshin@gmail.com>
2025-09-27 14:27:47 +0330

    Services created.

---
+++ OrderSystem/Services/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using OrderSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Services
+{
+    internal interface IOrderService
+    {
+        void CreateOrder(Order order);
+    }
+}
---
+++ OrderSystem/Services/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using OrderSystem.Log;
+using OrderSystem.Models;
+using OrderSystem.Notifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Services
+{
+    internal class OrderService : IOrderService
+    {
+        private readonly ILogger _logger;
+        private readonly INotifier _notifier;
+
+        public OrderService(ILogger logger, INotifier notifier)
+        {
+            _logger = logger;
+            _notifier = notifier;
+        }
+        public void CreateOrder(Order order)
+        {
+            Console.WriteLine($"Order created for {order.Product}");
+            _logger.Log($"Order created: {order.Product}");
+            _notifier.SendMessage($"Your order for {order.Product} has been placed.");
+        }
+    }
+}
