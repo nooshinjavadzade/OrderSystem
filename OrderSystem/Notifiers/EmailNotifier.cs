7c2b8afe1340281e8b07b88b96bdd99a9722efa8
nooshinjavadzade <javadzadenooshin@gmail.com>
2025-09-27 14:21:15 +0330

    Notifiers created.

---
+++ OrderSystem/Notifiers/EmailNotifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Notifiers
+{
+    internal class EmailNotifier : INotifier
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"[Email] {message}");
+        }
+    }
+}
