fa5412fb5539c9368ddc669ceb76694a12d7774c
nooshinjavadzade <javadzadenooshin@gmail.com>
2025-09-27 14:09:43 +0330

    Model created.

---
+++ OrderSystem/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Models
+{
+    internal class Order
+    {
+        public string Product { get; set; }
+    }
+}
